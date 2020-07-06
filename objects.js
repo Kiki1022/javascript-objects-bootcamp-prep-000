5ea149e2c413186be46ad44c2381f84a6709b85a
elegant-delimiter-3950 <kinnari1022@yahoo.com>
2020-07-06 00:24:21 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {artistName: "Song Title"};
+
+
+
+  function updatePlaylist(playlist, artistName, songTitle)
+ 
+  {
+  playlist.artistName = ['Slowdive', 'My Bloody Valentine', 'Phil Ochs'];
+  return playlist;
+  }
+  
