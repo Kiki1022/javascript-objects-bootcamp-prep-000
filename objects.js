a100ee169b6b7740e76fa20710f4f60e72a43074
elegant-delimiter-3950 <kinnari1022@yahoo.com>
2020-07-06 02:11:42 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var playlist = {artistName: songTitle};
   function updatePlaylist(playlist, artistName, songTitle)
  
   {
- return playlist.assign({songTitle: 'Slow dive'}, {songTitle:'My Bloody Valentine'}, {songTitle:'Phil Ochs'});
+    return playlist.assign({songTitle: 'Slow dive'}, {songTitle:'My Bloody Valentine'}, {songTitle:'Phil Ochs'});
 
   }
   
