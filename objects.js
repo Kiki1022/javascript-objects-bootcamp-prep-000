cbb4250401d92ddcd72cd8d76ca866fe922b9882
elegant-delimiter-3950 <kinnari1022@yahoo.com>
2020-07-06 02:34:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var playlist = {Britney: "Toxic"};
   function updatePlaylist(playlist, artistName, songTitle)
  
   {
-    return Object.assign(playlist, {'Slow dive': songTitle }, {'My Bloody Valentine': songTitle}, {'Phil Ochs': songTitle});
+    return Object.assign(playlist, {'Slowdive': songTitle }, {'My Bloody Valentine': songTitle}, {'Phil Ochs': songTitle});
 
   }
   
